a1c59f450a3875ef934bb1976691cd718a27b07f
prajaktapakhale <Ppakhale5753@conestogac.on.ca>
2023-06-20 12:15:08 -0400

    final push

---
+++ ScaleneT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ScaleneT
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            //Prajakta Pradeep Pakhale - Student ID - 8895753
+            string result;
+            //orginal code
+            //if((firstAngle != secondAngle) && (secondAngle != thirdAngle))
+
+            //mutant
+            if ((firstAngle == secondAngle) && (secondAngle == thirdAngle))
+            {
+                result = "The triangle is SCALENE Triangle";
+            }
+            else
+            {
+                result = "NOT A SCALENE TRIANGLE";
+            }
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
---
+++ Triangle/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ScaleneT;
+
+namespace Triangle;
+public class TestClass1
+{
+    //Prajakta Pradeep Pakhale - Student ID - 8895753
+    [TestFixture]
+
+    public class TestClass1_1
+    {
+        [Test]
+        public void Valid_SCALENE_Triangle()
+        {
+
+            //Arrange
+            int firstAngle = 10;
+            int secondAngle = 20;
+            int thirdAngle = 30;
+
+            string expected = "The triangle is SCALENE Triangle";
+
+            //act
+            string actual = ScaleneT.Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
